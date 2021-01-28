72101ae3cc5fe0d4d9a5cfbad1ba31bfcc41c778
Federico Brunelli <federico.brunelli4@studio.unibo.it>
2021-01-28 02:08:42 +0100

    Exception2 completed

--- src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -12,7 +12,7 @@ public final class TestStrictBankAccount {
     /**
      * Used to test Exceptions on {@link StrictBankAccount}.
      */
-    @Test
+   @Test
     public void testBankOperations() {
         /*
          * 1) Creare due StrictBankAccountImpl assegnati a due AccountHolder a
@@ -23,5 +23,18 @@ public final class TestStrictBankAccount {
          * presenza di un id utente errato, oppure al superamento del numero di
          * operazioni ATM gratuite.
          */
+    	AccountHolder ah1 = new AccountHolder("fede", "bruno", 101);
+    	AccountHolder ah2 = new AccountHolder("davi", "pesce", 102);
+    	StrictBankAccount ba1 = new StrictBankAccount(101, 4000, 50);
+    	StrictBankAccount ba2 = new StrictBankAccount(102, 2000, 10);
+    	ba1.deposit(101, 200);
+    	ba2.deposit(100, 200);
+    	for (int i = 0; i < 20; i++) {
+    		System.out.println("ba2: " + ba2.getBalance());
+    		ba2.withdrawFromATM(102, 150);
+    	}
+    	
+    	ba2.withdraw(102, 2100);
+		System.out.println("\nba2: " + ba2.getBalance());
     }
 }
