418213e8c6e9c7c57b58c0521a21dece05932ad5
Federico Brunelli <federico.brunelli4@studio.unibo.it>
2021-01-28 02:09:56 +0100

    Add module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module lab06 {
+	exports it.unibo.oop.lab.collections2;
+	exports it.unibo.oop.lab06.generics1;
+	exports it.unibo.oop.lab.exception2;
+	exports it.unibo.oop.lab.exception1;
+	exports it.unibo.oop.lab.collections1;
+
+	requires junit;
+}
